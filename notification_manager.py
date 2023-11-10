489c6b269b7d1fc0bd8016b70a1389cbba11aac3
implaymo <136201112+implaymo@users.noreply.github.com>
2023-11-10 12:52:37 +0000

    Data spreadsheet updates correctly after checking new flights data. Biggest changes on data_manager.py.

--- notification_manager.py
+++ notification_manager.py
@@ -1,13 +1,13 @@
-# from twilio.rest import Client
-# import os
-#
-# class NotificationManager:
-#     #This class is responsible for sending notifications with the deal flight details.
-#     account_sid = os.environ["account_sid"]
-#     auth_token = os.environ["auth_token"]
-#     twilio_num = os.environ["twilio_num"]
-#     my_num = os.environ["my_num"]
-#     client = Client(account_sid, auth_token)
-#
-#     message = client.messages.create(body="New price!", from_=twilio_num, to=my_num)
+from twilio.rest import Client
+import os
+
+class NotificationManager:
+    #This class is responsible for sending notifications with the deal flight details.
+    account_sid = os.environ["account_sid"]
+    auth_token = os.environ["auth_token"]
+    twilio_num = os.environ["twilio_num"]
+    my_num = os.environ["my_num"]
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(body="New price!", from_=twilio_num, to=my_num)
 
