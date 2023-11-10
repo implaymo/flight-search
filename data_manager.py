19ab8a3f26da4a6ca819ed8e9df6204614dc9009
implaymo <136201112+implaymo@users.noreply.github.com>
2023-11-10 12:29:19 +0000

    Data spreadsheet updates correctly after checking new flights data. Biggest changes on data_manager.py.

--- data_manager.py
+++ data_manager.py
@@ -3,26 +3,11 @@ from flight_data import FlightData
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     def __init__(self):
-        self.row = 1
         self.rows_data = []
+
+        self.sheet_prices = []
+        self.data_prices = []
         self.flight_data = FlightData()
-
-    def put_spreadsheet(self):
-        """Edits row from spreadsheet"""
-        for city_info in self.flight_data.city_data_list:
-            self.row += 1
-            data_for_spreadsheet = {
-                "price":
-                    {
-                        "city": city_info["city"],
-                        "iata": city_info["iata"],
-                        "lowest": city_info["price"]
-                    }
-
-            }
-            response = requests.put(url=f"https://api.sheety.co/3864f9e32cb6490ca4e1118527c50e15/flightDeals/prices/"
-                                        f"{self.row}", json=data_for_spreadsheet)
-            response.raise_for_status()
 
     def get_row_spreadsheet(self):
         """Compares prices from data with spreadsheet"""
@@ -35,12 +20,56 @@ class DataManager:
         data = response.json()
         for key in range(len(data["prices"])):
             city = data["prices"][key]["city"]
+            iata = data["prices"][key]["iata"]
             price = data["prices"][key]["lowest"]
             rows_flights_info = {
                 "city": city,
+                "iata": iata,
                 "price": price,
             }
             self.rows_data.append(rows_flights_info)
 
-    def compare_data(self):
+    def data_flight_api(self):
+        # Gets new flight data from API and stores as data_prices tuple list
+        for row in range(len(self.flight_data.city_data_list)):
+            new_lowest_price = (self.flight_data.city_data_list[row]["price"],
+                                self.flight_data.city_data_list[row]["iata"],
+                                self.flight_data.city_data_list[row]["city"])
+            self.data_prices.append(new_lowest_price)
+
+    def spreadsheet_data(self):
+        # Gets flight from spreadsheet
+        self.get_row_spreadsheet()
+        for row in range(len(self.rows_data)):
+            sheet_flight_price = (self.rows_data[row]["price"],
+                                  self.rows_data[row]["iata"],
+                                  self.rows_data[row]["city"])
+            self.sheet_prices.append(sheet_flight_price)
+
+    def price_compare(self):
+        """Gets price data from sheet and from flight api and compares it, in order to check which is lower"""
+        self.data_flight_api()
+        self.spreadsheet_data()
+        for new_price, new_iata, new_city in self.data_prices:
+            for price, iata, city in self.sheet_prices:
+                if new_city == city and new_price < price:
+                    self.put_spreadsheet(city, new_iata, new_price)
+
+    def put_spreadsheet(self, city, iata, price):
+        """Edits row from spreadsheet"""
+        for row, row_data in enumerate(self.rows_data):
+            if row_data["city"] == city:
+                data_for_spreadsheet = {
+                    "price":
+                        {
+                            "city": city,
+                            "iata": iata,
+                            "lowest": price
+                        }
+
+                }
+                response = requests.put(url=f"https://api.sheety.co/3864f9e32cb6490ca4e1118527c50e15/flightDeals/prices/"
+                                            f"{row + 2}", json=data_for_spreadsheet)
+                response.raise_for_status()
+
 
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from data_manager import DataManager
 
 data_manager = DataManager()
 
-data_manager.compare_spreadsheet()
+data_manager.price_compare()
